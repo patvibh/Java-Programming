a9a7e8fb176959abbc23c9448c1428beeea65fcc
Vibha Pateshwari <pateshwari.vibha@gmail.com>
2024-09-15 19:24:38 +0530

    Add files via upload

---
+++ typeCast.java
@@ -0,0 +1,13 @@
+class typeCast{
+    public static void main(String args[]){
+        // int x = 10;
+        // double y = x;
+
+        double x = 10.78;
+        int y = (int)x;
+        System.out.println("The value of x is ");
+        System.out.println(x);
+        System.out.println("The value of y is ");
+        System.out.println(y);
+    }
+}    
