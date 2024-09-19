c80a559f9ffa0d4e01ffda83d8925695a2bfe1f8
Vibha Pateshwari <pateshwari.vibha@gmail.com>
2024-09-19 10:25:24 +0530

    Add files via upload

---
+++ ConditionalStatements.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ConditionalStatements {
+    public static void main(String args[]){
+        boolean bl = true ;
+        if (2<3){
+            System.out.println("if is executed");
+        }
+        else {
+            System.out.println("if is not executed");
+        }
+    }
+}    
